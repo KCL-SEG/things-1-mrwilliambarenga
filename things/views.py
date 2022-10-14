2478fe22d178bb05696460dca909b2a3ddd931d5
mrwilliambarenga <williambarenga@gmail.com>
2022-10-14 20:31:16 +0100

    Refine the Thing model

--- things/views.py
+++ things/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    response = HttpResponse("home.html")
-    return response
+    return render(request, 'home.html')
